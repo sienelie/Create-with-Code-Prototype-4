daf2028e649abba8fe12ff9a2c3a7a3484bda350
Chrissie Ugay <chrissie.anne.k.ugay@accenture.com>
2022-07-22 15:51:30 +0800

    added textured enemy that follows the player around and spawns at random location on the island; spheres bounce off of each other; added a return function

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private GameObject player;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goalDirection = (player.transform.position - transform.position).normalized;
+        // Follow the player
+        // Normalized - so that the magnitude won't increase with respect to the enemys distance from the player 
+        playerRb.AddForce(goalDirection * speed);
+    }
+}
